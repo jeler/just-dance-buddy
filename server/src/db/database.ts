07a9dfca76a83caba47a308d4abc7e5f3604f4e6
Jenny <jennifer.eler1@gmail.com>
2024-06-06 16:56:26 -0500

    sequelize-typescript database config

--- server/src/db/database.ts
+++ server/src/db/database.ts
@@ -1,9 +1,15 @@
-import { Sequelize } from 'sequelize';
+import { Sequelize } from 'sequelize-typescript';
 import { DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER } from '../config/config';
+import { Song } from '../models/song';
 
-export default new Sequelize({
+const connection = new Sequelize({
     dialect: 'postgres',
-    database: DB_NAME,
+    host: DB_HOST,
     username: DB_USER,
-    password: DB_PASSWORD
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    logging: false,
+    models: [Song]
 });
+
+export default connection;
