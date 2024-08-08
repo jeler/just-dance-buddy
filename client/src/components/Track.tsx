0c8788748ce83a8551999bd7018c0c17ac5bc83b
Jenny <jennifer.eler1@gmail.com>
2024-08-07 23:02:06 -0500

    FE: track list table styling

--- client/src/components/Track.tsx
+++ client/src/components/Track.tsx
@@ -3,8 +3,12 @@ import { ITrack } from "../interfaces/Track";
 export const Track = ({ track }: { track: ITrack }) => {
   return (
     <tr key={track.track_id}>
-      <td>{track.song?.song_name}</td>
-      <td>{track.song?.artist}</td>
+      <td>
+        <span className="px-2">{track.song?.song_name}</span>
+      </td>
+      <td>
+        <span className="px-2">{track.song?.artist}</span>
+      </td>
     </tr>
   );
 };
--- client/src/components/TrackList.tsx
+++ client/src/components/TrackList.tsx
@@ -3,15 +3,22 @@ import { Track } from "./Track";
 
 export const TrackList = ({ tracks }: { tracks: ITrack[] }) => {
   return (
-    <table className="table-auto shadow-lg bg-white">
-      <thead>
+    <table className="table-auto shadow-lg bg-white w-screen empty-cells-hidden border-spacing-1">
+      <caption className="caption-top">Search Results</caption>
+      <thead className="rounded-tl bg-gray-200">
         <tr>
-          <th>Song</th>
-          <th>Artist</th>
-          <th></th>
+          <th className="px-2 text-left font-medium text-gray-500 uppercase tracking-wider">
+            Song
+          </th>
+          <th className="px-2 text-left font-medium text-gray-500 uppercase tracking-wider">
+            Artist
+          </th>
+          <th className="px-2 text-left font-medium text-gray-500 uppercase tracking-wider">
+            Track
+          </th>
         </tr>
       </thead>
-      <tbody>
+      <tbody className="px-2">
         {tracks.map((t) => (
           <Track track={t} />
         ))}
