3dbaabe9b249aa99a9229fdc53cbd99163361711
Jenny <jennifer.eler1@gmail.com>
2024-11-04 12:46:39 -0600

    formatting

---
+++ client/src/components/base-layout/LeftSideBar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LeftSideBar = ({ children }) => {
+  return (
+    <>
+      <div className="left-sidebar">{children}</div>
+    </>
+  );
+};
+
+export default LeftSideBar;
